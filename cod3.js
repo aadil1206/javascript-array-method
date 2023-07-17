21c3abd982da2b976cff16b25348208556ca92ec
aahiraashrafi <adilmohammadkgn786@gmail.com>
2023-07-17 12:28:39 +0530

    commit

--- cod3.js
+++ cod3.js
@@ -1,52 +1,70 @@
-const users=[
+const users = [
     {
-        id:1,
-        name:"aadil",
-        isActive:true,
-        age:20,
-        bool:false
-    }, 
-    {
-        id:2,
-        name:"ajaz",
-        isActive:false,
-        age:28,
-        bool:true
+        id: 1,
+        name: "aadil",
+        isActive: true,
+        age: 20,
+        boolean: false
     },
     {
-        id:3,
-        name:"Noor",
-        isActive:true,
-        age:50,
-        bool:false
+        id: 2,
+        name: "ajaz",
+        isActive: false,
+        age: 28,
+        boolean: true
     },
     {
-        id:4,
-        name:"sahil",
-        isActive:false,
-        age:15,
-        bool:true
+        id: 3,
+        name: "Noor",
+        isActive: true,
+        age: 50,
+        boolean: false
+    },
+    {
+        id: 4,
+        name: "sahil",
+        isActive: false,
+        age: 15,
+        boolean: true
     },
 ]
 //write code to get array of names from given array of users
 
-let array=[]
-  users.filter((item)=>!item.bool).
-map((item)=>{
-    array.push(item.name)
+let array = []
+//using for loop
+for (let i = 0; i < users.length; i++) {
+    array.push(users[i].name)
 }
-)
-console.log(array)
-// for (let i=0;i<users.length;i++){
-//     if(users[i].bool){
-//      array.push(users[i].name)}
-// }
+//using foreach loop
 
-// users.forEach((item)=> {
-//     if(item.bool){
-//     array.push(item.name)}
-// });
-// // users.map((item)=>{
-// //     array.push(item.name)
-// // })
-// console.log(array)
+users.forEach((item)=> {
+    array.push(item.name)
+});
+
+//using map function
+
+users.map((item)=>{
+    array.push(item.name)
+})
+ 
+// write a code to find boolean is true
+//using for loop
+
+for (let i = 0; i < users.length; i++) {
+    if(users[i].boolean){
+    array.push(users[i].name)}
+}
+//using foreach loop
+
+users.forEach((item)=> {
+    if(item.boolean){
+    array.push(item.name)}
+});
+//using map function and filter 
+
+users.filter((item)=>{
+    item.boolean
+})
+.map((item)=>{
+    array.push(item.name)
+})
