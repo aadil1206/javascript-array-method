19688f47f1de856ca6153006298e29fa102cdc76
aahiraashrafi <adilmohammadkgn786@gmail.com>
2023-07-18 16:20:08 +0530

    commit

--- cod3.js
+++ cod3.js
@@ -37,78 +37,112 @@ for (let i = 0; i < users.length; i++) {
 }
 //using foreach loop
 
-users.forEach((item)=> {
+users.forEach((item) => {
     array.push(item.name)
 });
 
 //using map function
 
-users.map((item)=>{
+users.map((item) => {
     array.push(item.name)
 })
- 
+
 // write a code to find boolean is true
 //using for loop
 
 for (let i = 0; i < users.length; i++) {
-    if(users[i].boolean){
-    array.push(users[i].name)}
+    if (users[i].boolean) {
+        array.push(users[i].name)
+    }
 }
 //using foreach loop
 
-users.forEach((item)=> {
-    if(item.boolean){
-    array.push(item.name)}
+users.forEach((item) => {
+    if (item.boolean) {
+        array.push(item.name)
+    }
 });
 //using map function and filter 
 
-users.filter((item)=>{
+users.filter((item) => {
     item.boolean
 })
-.map((item)=>{
-    array.push(item.name)
-})
+    .map((item) => {
+        array.push(item.name)
+    })
 
 //write a code for age greater than 20
 //using for loop
 
 for (let i = 0; i < users.length; i++) {
-    if(users[i].age>20){
-    array.push(users[i].age)}
+    if (users[i].age > 20) {
+        array.push(users[i].age)
+    }
 }
 //using foreach loop
 
-users.forEach((item)=> {
-    if(item.age>20){
-    array.push(item.age)}
+users.forEach((item) => {
+    if (item.age > 20) {
+        array.push(item.age)
+    }
 });
 //using map function and filter 
 
-users.filter((item)=>{
-    item.age>20
+users.filter((item) => {
+    item.age > 20
 })
-.map((item)=>{
-    array.push(item.age)
-})
+    .map((item) => {
+        array.push(item.age)
+    })
 // write a function to sort letter
 
-let letters=['a','c','b','x','d','f','Z','M','S']
+let letters = ['a', 'c', 'b', 'x', 'd', 'f', 'Z', 'M', 'S']
 letters.sort();
 // console.log(letters)
-let number=[9,4,6,7,8,10,1,20]
+let number = [9, 4, 6, 7, 8, 10, 1, 20]
 number.sort();
 // console.log(number)
- 
+
 //sort it effectively ascending order
-let number1=[9,4,6,7,8,10,1,20]
-number1.sort((a,b)=>{
-    return a-b
+let number1 = [9, 4, 6, 7, 8, 10, 1, 20]
+number1.sort((a, b) => {
+    return a - b
 });
 // console.log(number1)
 
 //sort it effectively descending order
-let number2=[9,4,6,7,8,10,1,20]
-number2.sort((a,b)=>{
-    return b-a
+let number2 = [9, 4, 6, 7, 8, 10, 1, 20]
+number2.sort((a, b) => {
+    return b - a
 });
-console.log(number2)
+// console.log(number2)
+const items = [
+    { name: "Edward", value: 21 },
+    { name: "Sharpe", value: 37 },
+    { name: "And", value: 45 },
+    { name: "The", value: -12 },
+    { name: "Magnetic", value: 13 },
+    { name: "Zeros", value: 37 },
+];
+
+// sort with value
+items.sort((a, b) => {
+    return a.value - b.value
+})
+//   console.log(items)
+
+//sort with names
+items.sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+
+    // names must be equal
+    return 0;
+
+});
